cf210be75abc1ff3c36c5f3e46ba2489946a69f0
Meder <m.toktosunov@icloud.com>
2015-12-03 09:21:38 -0500

    initial

---
+++ public/javascripts/app.js
@@ -0,0 +1,13 @@
+
+$('#get-dish').on('click', function(event) {
+  event.preventDefault();
+
+  var request = $.ajax({
+    method: "GET",
+    url: "/dishes/random.json"
+  });
+
+  request.done(function(alertDish) {
+    alert(alertDish['dish']);
+  });
+});
---
+++ test.rb
@@ -0,0 +1,21 @@
+DISHES = [
+  "Roasted Brussels Sprouts",
+  "Fresh Brussels Sprouts Soup",
+  "Brussels Sprouts with Toasted Breadcrumbs, Parmesan, and Lemon",
+  "Cheesy Maple Roasted Brussels Sprouts and Broccoli with Dried Cherries",
+  "Hot Cheesy Roasted Brussels Sprout Dip",
+  "Pomegranate Roasted Brussels Sprouts with Red Grapes and Farro",
+  "Roasted Brussels Sprout and Red Potato Salad",
+  "Smoky Buttered Brussels Sprouts",
+  "Sweet and Spicy Roasted Brussels Sprouts",
+  "Smoky Buttered Brussels Sprouts",
+  "Brussels Sprouts and Egg Salad with Hazelnuts"
+
+]
+
+
+def random_dish
+  DISHES.sample
+end
+
+return random_dish
